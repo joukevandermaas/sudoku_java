a31f46ca4652e30e9ea9f4f4a25fd883599fb9cf
Koen Keune <koenkeune@live.nl>
2013-03-18 01:29:11 +0100

    row werkend met prints

--- src/DoubleLockedStrategy.java
+++ src/DoubleLockedStrategy.java
@@ -22,7 +22,7 @@ public class DoubleLockedStrategy extends LockedStrategy{
 					int squareRow = i / squaresPerRow;
 					int j = 1;
 					int squareRowNext = (i+j) / squaresPerRow;
-					System.out.print("squareRow: "+squareRow+" squareRowNext: "+squareRowNext+"\n");
+					//System.out.print("squareRow: "+squareRow+" squareRowNext: "+squareRowNext+"\n");
 					SquareCellContainer square2 = null;
 					
 					while (squareRow==squareRowNext) {
@@ -31,24 +31,27 @@ public class DoubleLockedStrategy extends LockedStrategy{
 						List<Integer> rows2 = square2.findActivatedRows(n);
 						if (rows2.size() == 2) {
 							if (rows.get(0)==rows2.get(0) && rows.get(1)==rows2.get(1)) {
+								System.out.print("matching square\n");
 								int startCol = getRealCol(i, 0);
-								int startCol2 = getRealCol(j, 0);
-								int deleteRow = rows.get(0) + rows.get(1);
+								int startCol2 = getRealCol(i+j, 0);
+								System.out.print("square1: "+i+" square2: "+j+"\n");
+								System.out.print("row1: "+rows.get(0)+" row2: "+rows.get(1)+"\n");
+								/*int deleteRow = rows.get(0) + rows.get(1);
 								if (deleteRow == 1) {
 									deleteRow = 2;
 								} else if (deleteRow == 2) {
 									deleteRow = 1;
 								} else {
 									deleteRow = 0;
-								}
+								}*/
+								System.out.print("realRow: "+getRealRow(i, rows.get(1))+"\n\n");	
 									
-									
-								Cell[] row = puzzle.getRows()[getRealRow(i, deleteRow)]
+								Cell[] row = puzzle.getRows()[getRealRow(i, rows.get(1))]
 										.getCells();
-								//row + row
+
 								if (removePossibilities(row, n, startCol, startCol
 										+ Sudoku.SQUARE_ROWS, startCol2, startCol2 + Sudoku.SQUARE_ROWS))
-									result = true;
+									return true;
 							}
 						}
 						
@@ -63,14 +66,17 @@ public class DoubleLockedStrategy extends LockedStrategy{
 		return result;
 	}
 	
-	protected boolean removePossibilities(Cell[] cells, int value,
+	private boolean removePossibilities(Cell[] cells, int value,
 			int lowerBound, int upperBound, int lowerBound2, int upperBound2)
 					throws SudokuException {
 		boolean result = false;
 		for (int i = 0; i < Sudoku.SUDOKU_SIZE; i++) {
 			if ((i < lowerBound || i >= upperBound) && i < lowerBound2 || i >= upperBound2) {
-				//System.out.print("\ncell: "+cells[i].getPossibilities());
+				System.out.print("value: "+value+"\n");
+				System.out.print("i: "+i+"\n");
+				System.out.print("hasvalue: "+cells[i].hasValue()+"\n\n");
 				if (!cells[i].hasValue() && cells[i].removePossibility(value)) {
+					System.out.print("value: "+value+"\n");
 					result = true;
 				}
 					
