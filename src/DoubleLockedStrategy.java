e5c738f4759af09dcb4e4f92a7c258f870f24e70
Koen Keune <koenkeune@live.nl>
2013-03-18 02:26:18 +0100

    werkt voor iig 1 geval

--- src/DoubleLockedStrategy.java
+++ src/DoubleLockedStrategy.java
@@ -1,102 +1,131 @@
+/*
+ * DoubleLockedStrategy.java
+ * Werkt voor 1 geval, weet niet of 'col if' werkt
+ * 
+ * Version information
+ * v0.2 (alpha 1)
+ *
+ * Date
+ * 18/03/2013
+ * 
+ * Author
+ * Jouke van der Maas & Koen Keune
+ * 
+ */
+
 import java.util.List;
 
-public class DoubleLockedStrategy extends LockedStrategy{
+public class DoubleLockedStrategy extends LockedStrategy {
 
 	@Override
 	public boolean removePossibilities(Sudoku puzzle) throws SudokuException {
 		CellContainer[] squares = puzzle.getSquares();
-		boolean result = false;
+		// boolean result = false;
 		for (int i = 0; i < squares.length; i++) { // for every square
 			SquareCellContainer square = (SquareCellContainer) squares[i];
 
-			
 			for (int n = 1; n <= Sudoku.SUDOKU_SIZE; n++) { // for every digit
 				List<Integer> rows = square.findActivatedRows(n);
 				List<Integer> cols = square.findActivatedColumns(n);
-
 				if (rows.size() == 2) {
-					//findNextDoubleRow(i,squareRow1,squareRow2);
-					System.out.print("digit: "+ n + " square: "+i+"\n");
-					
-					int squaresPerRow = Sudoku.SUDOKU_SIZE / Sudoku.SQUARE_COLUMNS;
-					int squareRow = i / squaresPerRow;
-					int j = 1;
-					int squareRowNext = (i+j) / squaresPerRow;
-					//System.out.print("squareRow: "+squareRow+" squareRowNext: "+squareRowNext+"\n");
-					SquareCellContainer square2 = null;
-					
-					while (squareRow==squareRowNext) {
-						square2 = (SquareCellContainer) squares[i+j];
-						
-						List<Integer> rows2 = square2.findActivatedRows(n);
-						if (rows2.size() == 2) {
-							if (rows.get(0)==rows2.get(0) && rows.get(1)==rows2.get(1)) {
-								System.out.print("matching square\n");
-								int startCol = getRealCol(i, 0);
-								int startCol2 = getRealCol(i+j, 0);
-								System.out.print("square1: "+i+" square2: "+j+"\n");
-								System.out.print("row1: "+rows.get(0)+" row2: "+rows.get(1)+"\n");
-								/*int deleteRow = rows.get(0) + rows.get(1);
-								if (deleteRow == 1) {
-									deleteRow = 2;
-								} else if (deleteRow == 2) {
-									deleteRow = 1;
-								} else {
-									deleteRow = 0;
-								}*/
-								System.out.print("realRow: "+getRealRow(i, rows.get(1))+"\n\n");	
-									
-								Cell[] row = puzzle.getRows()[getRealRow(i, rows.get(1))]
-										.getCells();
+					int startCol2 = findNextRowSquare(i, n, rows, squares);
+					if (startCol2 != -1) {
+						int startCol = getRealCol(i, 0);
+						for (int j = 0; j < 2; j++) {
+							Cell[] row = puzzle.getRows()[getRealRow(i,
+									rows.get(j))].getCells();
 
-								if (removePossibilities(row, n, startCol, startCol
-										+ Sudoku.SQUARE_ROWS, startCol2, startCol2 + Sudoku.SQUARE_ROWS))
-									return true;
-							}
+							if (removePossibilities(row, n, startCol, startCol
+									+ Sudoku.SQUARE_ROWS, startCol2, startCol2
+									+ Sudoku.SQUARE_ROWS))
+								return true; // or result = true
 						}
-						
-						j++;
-						squareRowNext = (i+j) / squaresPerRow;
 					}
-					
+				}
+				if (cols.size() == 2) {
+					int startRow2 = findNextColSquare(i, n, rows, squares);
+					if (startRow2 != -1) {
+						int startRow = getRealRow(i, 0);
+						for (int j = 0; j < 2; j++) {
+							Cell[] row = puzzle.getColumns()[getRealCol(i,
+									rows.get(j))].getCells();
+
+							if (removePossibilities(row, n, startRow, startRow
+									+ Sudoku.SQUARE_ROWS, startRow2, startRow2
+									+ Sudoku.SQUARE_ROWS))
+								return true; // or result = true
+						}
+					}
 				}
 			}
-			
 		}
-		return result;
+		return false; // or return result
 	}
-	
+
+	// double bound version
 	private boolean removePossibilities(Cell[] cells, int value,
 			int lowerBound, int upperBound, int lowerBound2, int upperBound2)
-					throws SudokuException {
+			throws SudokuException {
 		boolean result = false;
 		for (int i = 0; i < Sudoku.SUDOKU_SIZE; i++) {
-			if ((i < lowerBound || i >= upperBound) && i < lowerBound2 || i >= upperBound2) {
-				System.out.print("value: "+value+"\n");
-				System.out.print("i: "+i+"\n");
-				System.out.print("hasvalue: "+cells[i].hasValue()+"\n\n");
-				if (!cells[i].hasValue() && cells[i].removePossibility(value)) {
-					System.out.print("value: "+value+"\n");
+			if ((i < lowerBound || i >= upperBound) && i < lowerBound2
+					|| i >= upperBound2) {
+				if (!cells[i].hasValue() && cells[i].removePossibility(value))
 					result = true;
-				}
-					
-					
-					
 			}
 		}
 		return result;
 	}
-}
-	/*public int findNextDoubleRow(int square, int row1, int row2) {
+
+	private int findNextRowSquare(int squareNum, int digit, List<Integer> rows,
+			CellContainer[] squares) throws SudokuException {
 		int squaresPerRow = Sudoku.SUDOKU_SIZE / Sudoku.SQUARE_COLUMNS;
-		int bla = square / squaresPerRow;
-		int i = 0;
-		int bla2 = (square+i) / squaresPerRow;
-		while (bla==bla2) {
-			i++;
-			square+i;
+		int squareRow = squareNum / squaresPerRow;
+		int j = 1;
+		int squareRowNext = (squareNum + j) / squaresPerRow;
+
+		SquareCellContainer square2 = null;
+
+		while (squareRow == squareRowNext) { // checks if next square is on the
+												// same row
+			square2 = (SquareCellContainer) squares[squareNum + j];
+
+			List<Integer> rows2 = square2.findActivatedRows(digit);
+			if (rows2.size() == 2) {
+				if (rows.get(0) == rows2.get(0) && rows.get(1) == rows2.get(1)) {
+					return getRealCol(squareNum + j, 0);
+				}
+			}
+
+			j++; // next square
+			squareRowNext = (squareNum + j) / squaresPerRow;
 		}
-		return 0;
-	}*/
+		return -1;
+	}
 
+	private int findNextColSquare(int squareNum, int digit, List<Integer> cols,
+			CellContainer[] squares) throws SudokuException {
+		int squaresPerCol = Sudoku.SUDOKU_SIZE / Sudoku.SQUARE_ROWS;
+		int squareCol = squareNum % squaresPerCol;
+		int j = 1;
+		int squareColNext = (squareNum + j) / squaresPerCol;
 
+		SquareCellContainer square2 = null;
+
+		while (squareCol == squareColNext) { // checks if next square is on the
+												// same row
+			square2 = (SquareCellContainer) squares[squareNum + j];
+
+			List<Integer> cols2 = square2.findActivatedColumns(digit);
+			if (cols2.size() == 2) {
+				if (cols.get(0) == cols2.get(0) && cols.get(1) == cols2.get(1)) {
+					return getRealRow(squareNum + j, 0);
+				}
+			}
+
+			j++; // next square
+			squareColNext = (squareNum + j) % squaresPerCol;
+		}
+		return -1;
+	}
+}
